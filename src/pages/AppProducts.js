0b0f6b2705130815c5a42b36f6f5a56bead32f8b
BiljanaDa <bibidakovic@gmail.com>
2024-01-22 00:00:19 +0100

    filtering products by name

--- src/pages/AppProducts.js
+++ src/pages/AppProducts.js
@@ -3,12 +3,24 @@ import ProductService from "../service/ProductService";
 
 export default function AppProducts() {
   const products = ProductService.getAll();
+  const [search, setSearch] = useState("");
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="container mt-4">
       <h1 className="mb-4">Product List</h1>
-      <ul className="list-group list-group-flush">
-        {products.map((product) => (
+      <input
+        type="text"
+        className="form-control form-control-sm py-2 w-50 mb-3"
+        placeholder="Search by product name"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <ul className="list-group list-group-flush w-50">
+        {filteredProducts.map((product) => (
           <li
             key={product.id}
             className="list-group-item d-flex justify-content-between align-items-center"
